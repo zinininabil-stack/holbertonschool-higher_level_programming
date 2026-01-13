e05deb12fd70f5f4e8e1b1a20abddc6857f67f09
Nabil Zinini <zinini.nabil@gmail.com>
2026-01-13 14:29:15 +0100

    Fix last letter slicing in 7-edges.py to correctly display the last two letters

--- python-hello_world/7-edges.py
+++ python-hello_world/7-edges.py
@@ -2,5 +2,5 @@
 word = "Holberton"
 # YOUR CODE GOES HERE. PLEASE REMOVE THIS LINE
 print(f"First 3 letters: {word[:3]}")
-print(f"Last 2 letters: {word[7:]}")
+print(f"Last 2 letters: {word[-2:]}")
 print(f"Middle word: {word[1:-1]}")
