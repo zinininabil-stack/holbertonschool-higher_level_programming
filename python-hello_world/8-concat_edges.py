ec88411f1b7b460dea75b6e2650decb59cbeef95
Nabil Zinini <zinini.nabil@gmail.com>
2026-01-13 18:22:34 +0100

    Refactor 8-concat_edges.py to remove commented line and streamline string assignment

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-# Print object-oriented programming with Python using only slicing
-print(str[39:67] + str[107:112] + str[0:6])
+str = str[39:67] + str[107:112] + str[0:6]
+print(str)
