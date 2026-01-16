7a07d6bbc54d6c4827687058580a6f3f4af31c57
Nabil Zinini <zinini.nabil@gmail.com>
2026-01-16 16:22:49 +0100

    Add 4-hidden_discovery.py to list non-private attributes of hidden_4 module

---
+++ python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+import hidden_4
+
+if __name__ == "__main__":
+
+    names = dir(hidden_4)
+    names_alpha = sorted(names)
+    for name in names_alpha:
+        if name.startswith("__"):
+            continue
+        print(name)
