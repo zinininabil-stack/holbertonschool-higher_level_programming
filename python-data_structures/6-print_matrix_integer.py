5e498de581d4ef8a316a2a12a34e1acaa71bb9aa
zinininabil-stack <zinini.nabil@gmail.com>
2026-01-20 11:42:52 +0100

    Remove example matrix and function calls from print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -6,13 +6,3 @@ def print_matrix_integer(matrix=[[]]):
                 print(" ", end="")
             print("{:d}".format(element), end="")
         print()
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
