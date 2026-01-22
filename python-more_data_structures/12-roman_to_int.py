bdb79c9cc8dff51b1379bc597747f5e4a68358d6
zinininabil-stack <zinini.nabil@gmail.com>
2026-01-22 13:58:03 +0100

    Refactor input validation and formatting in roman_to_int.py

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None or type(roman_string) != str:
+    if not isinstance(roman_string, str):
         return 0
-    
-    convertor = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    convertor = {"I": 1, "V": 5, "X": 10, "L": 50,
+                 "C": 100, "D": 500, "M": 1000}
     total = 0
-    
+
     for i in range(len(roman_string)):
         current_value = convertor[roman_string[i]]
-        
+
         if i < len(roman_string) - 1:
             next_value = convertor[roman_string[i + 1]]
-            
+
             if current_value < next_value:
                 total -= current_value
             else:
                 total += current_value
         else:
             total += current_value
-    
+
     return total
