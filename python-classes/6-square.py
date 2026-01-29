7f4ee90045418f38f5d934175a577cde0cdc498b
zinininabil-stack <zinini.nabil@gmail.com>
2026-01-29 09:56:52 +0100

    Add Square class with size and position validation, area calculation, and print method

---
+++ python-classes/6-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Class that defines a square with private size and position attributes"""
+
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Getter for size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter for size with validation"""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """Getter for position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Setter for position with validation"""
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def area(self):
+        """Public instance method that returns the current square area"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square with the character # at given position"""
+        if self.__size == 0:
+            print()
+            return
+
+        # Print vertical offset (position[1])
+        for i in range(self.__position[1]):
+            print()
+
+        # Print the square with horizontal offset (position[0])
+        for i in range(self.__size):
+            print(" " * self.__position[0] + "#" * self.__size)
+
+my_square_1 = Square(3)
+my_square_1.my_print()
+
+print("--")
+
+my_square_2 = Square(3, (1, 1))
+my_square_2.my_print()
+
+print("--")
+
+my_square_3 = Square(3, (3, 0))
+my_square_3.my_print()
+
+print("--")
+        
