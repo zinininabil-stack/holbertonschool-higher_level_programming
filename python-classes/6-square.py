ac2e1dca94ff696dd614ca8adc4659dcd28a973f
zinininabil-stack <zinini.nabil@gmail.com>
2026-01-29 10:02:02 +0100

    Remove example usage of Square class from 6-square.py

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -54,19 +54,3 @@ class Square:
         # Print the square with horizontal offset (position[0])
         for i in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
-
-my_square_1 = Square(3)
-my_square_1.my_print()
-
-print("--")
-
-my_square_2 = Square(3, (1, 1))
-my_square_2.my_print()
-
-print("--")
-
-my_square_3 = Square(3, (3, 0))
-my_square_3.my_print()
-
-print("--")
-        
