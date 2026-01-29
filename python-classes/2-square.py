0bb1cd3c8bc3a9608427d7b6726739c6b1912bca
zinininabil-stack <zinini.nabil@gmail.com>
2026-01-29 09:56:03 +0100

    Add Square class with size validation and constructor

---
+++ python-classes/2-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Class that defines a square with private size attribute and validation"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+        
