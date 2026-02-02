7d125664f3eba05caeac609c42d88d943ee5e092
zinininabil-stack <zinini.nabil@gmail.com>
2026-02-02 14:01:01 +0100

    Add Rectangle class that inherits from BaseGeometry with validation

---
+++ python-inheritance/8-rectangle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module that defines Rectangle class"""
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """A rectangle class that inherits from BaseGeometry"""
+    
+    def __init__(self, width, height):
+        """Initialize a rectangle with width and height
+        
+        Args:
+            width (int): width of the rectangle
+            height (int): height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
