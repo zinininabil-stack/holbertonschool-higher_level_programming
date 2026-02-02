4e771b5d2f83a57e0e5ffce6cfe51dc712f9799d
zinininabil-stack <zinini.nabil@gmail.com>
2026-02-02 13:29:23 +0100

    Add BaseGeometry class with module docstring for geometry representation

---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module defines an empty geometry class for demonstration purposes.
+"""
+
+
+class BaseGeometry:
+    """
+    Represents an empty class with no attributes or methods.
+    """
+    pass
