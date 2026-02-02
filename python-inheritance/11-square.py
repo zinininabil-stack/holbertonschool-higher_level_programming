c52b37f351cadb4cfa79fa30bdcd861aa7e86fa1
zinininabil-stack <zinini.nabil@gmail.com>
2026-02-02 15:28:34 +0100

    Add Square class that inherits from Rectangle with size validation and string representation

---
+++ python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module that defines Square class"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """A square class that inherits from Rectangle"""
+    
+    def __init__(self, size):
+        """Initialize a square with size
+        
+        Args:
+            size (int): size of the square
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """Returns string representation of the square"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
+    
