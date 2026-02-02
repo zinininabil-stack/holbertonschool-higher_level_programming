faa8862e7d2e703c97fbd5b9fec5fb2ea026385c
zinininabil-stack <zinini.nabil@gmail.com>
2026-02-02 13:23:40 +0100

    Fix module docstring and improve is_kind_of_class function description

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python3
-"""Module that defines is_same_class function"""
+"""Module that defines is_kind_of_class function"""
+
 
 def is_kind_of_class(obj, a_class):
-    """Returns True if obj is kind of an instance of a_class
+    """Returns True if obj is an instance of, or inherited from, a_class
     
     Args:
         obj: object to check
         a_class: class to compare against
         
     Returns:
-        True if obj is exactly an instance of a_class or and instance of, False otherwise"""
-    
-    return type(obj) is a_class or isinstance
+        True if obj is an instance of a_class or inherited from it
+    """
+    return isinstance(obj, a_class)
