07b3bd544ae43e8549bcd2ff0582f11dcddf5396
zinininabil-stack <zinini.nabil@gmail.com>
2026-02-04 10:43:50 +0100

    Add docstring to Square class and its initializer for clarity

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -6,8 +6,14 @@ Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """"""
+    """A square class that inherits from Rectangle"""
+    
     def __init__(self, size):
+        """Initialize a square with size
+        
+        Args:
+            size (int): size of the square
+        """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
