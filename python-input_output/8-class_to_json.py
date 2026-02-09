78ca7621b81125c5eded5ad6c00ba4d93ee2fa95
zinininabil-stack <zinini.nabil@gmail.com>
2026-02-09 13:42:58 +0100

    Update 8-class_to_json.py: enhance docstring for class_to_json function

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
-""""""
+"""Module that defines class_to_json function"""
+
+
 def class_to_json(obj):
+    """Returns the dictionary description for JSON serialization of an object
+    
+    Args:
+        obj: instance of a Class
+        
+    Returns:
+        dict: dictionary with simple data structure (list, dict, str, int, bool)
+    """
     return obj.__dict__
