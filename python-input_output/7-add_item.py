c21ca81431d1a99a75d8dd0aecfe4c5c34de75b4
zinininabil-stack <zinini.nabil@gmail.com>
2026-02-09 12:55:45 +0100

    Add 7-add_item.py: implement a script to add command line arguments to a list and save it to a JSON file

---
+++ python-input_output/7-add_item.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Script that adds arguments to a Python list and saves it to a JSON file"""
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+
+try:
+    my_list = load_from_json_file(filename)
+except FileNotFoundError:
+    my_list = []
+
+my_list.extend(sys.argv[1:])
+save_to_json_file(my_list, filename)
