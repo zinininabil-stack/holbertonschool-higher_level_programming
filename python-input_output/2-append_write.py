4b2befeb173b48348b0ce31ff6e04a50211db719
zinininabil-stack <zinini.nabil@gmail.com>
2026-02-13 18:12:44 +0100

    Fix docstring and improve file handling in append_write function

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""""""
+"""Module to append text to a file."""
+
+
 def append_write(filename="", text=""):
-    with open('filename', 'a', encoding="utf-8") as f:
-        return f.write(text)
+    """Appends text to a file and returns the number of characters added."""
+    with open(filename, 'a', encoding='utf-8') as file:
+        return file.write(text)
