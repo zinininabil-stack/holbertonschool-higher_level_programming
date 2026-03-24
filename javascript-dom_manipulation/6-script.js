0698d1c55a91a853702eab439921559aec221c1f
zinininabil-stack <zinini.nabil@gmail.com>
2026-03-24 10:17:17 +0100

    Add script to fetch and display character name from API

---
+++ javascript-dom_manipulation/6-script.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+fetch('https://swapi-api.hbtn.io/api/people/5/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    document.querySelector('#character').textContent = data.name;
+  });
