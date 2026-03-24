b0fbb6e55be96bf25795136cb3f466efc03100a9
zinininabil-stack <zinini.nabil@gmail.com>
2026-03-24 10:07:26 +0100

    Add 3-script.js to toggle header color between red and green on click

---
+++ javascript-dom_manipulation/3-script.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+document.querySelector('#toggle_header').addEventListener('click', () => {
+  const header = document.querySelector('header');
+  if (header.classList.contains('red')) {
+    header.classList.remove('red');
+    header.classList.add('green');
+  } else {
+    header.classList.remove('green');
+    header.classList.add('red');
+  }
+});
